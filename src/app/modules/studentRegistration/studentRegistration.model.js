9a1bff363ac0bb1623683fa41fda4be24950cb4f
Rasalahmad <rasalahmadsit@gmail.com>
2023-12-31 23:38:22 +0600

    wip

--- src/app/modules/studentRegistration/studentRegistration.model.js
+++ src/app/modules/studentRegistration/studentRegistration.model.js
@@ -6,6 +6,10 @@ const studentRegistrationSchema = new Schema(
       type: String,
       required: true,
     },
+    studentId: {
+      type: String,
+      required: true,
+    },
     image: {
       type: String,
       required: true,
